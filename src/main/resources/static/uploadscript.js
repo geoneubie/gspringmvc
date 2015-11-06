ef1254b3b44d14435d0e746ab0b37891e1a92657
geoneubie <geoneubie@gmail.com>
2015-11-06 14:16:21 -0700

    JQuery implementation.

---
+++ src/main/resources/static/uploadscript.js
@@ -0,0 +1,17 @@
+function submitForm() {
+    console.log("submit event");
+    var fd = new FormData(document.getElementById("fileinfo"));
+    fd.append("label", "WEBUPLOAD");
+    $.ajax({
+      url: "/fileupload/upload",
+      type: "POST",
+      data: fd,
+      enctype: 'multipart/form-data',
+      processData: false,  // tell jQuery not to process the data
+      contentType: false   // tell jQuery not to set contentType
+    }).done(function( data ) {
+        console.log("Output:");
+        console.log( data );
+    });
+    return false;
+}
