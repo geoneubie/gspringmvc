a36202c6c70ae9f960b1d874d5c56b183e5aa845
geoneubie <geoneubie@gmail.com>
2015-11-24 17:59:34 -0700

    Reorganized cient code updated form object with additional metadata fields.

--- src/main/groovy/controller/FileUploadController.groovy
+++ src/main/groovy/controller/FileUploadController.groovy
@@ -16,22 +16,22 @@ import org.springframework.web.multipart.MultipartFile
 public class FileUploadController {
 
     @RequestMapping(value="/upload", method=RequestMethod.POST)
-    public @ResponseBody String handleFileUpload(@RequestParam("file") MultipartFile file){
+    public @ResponseBody String handleFileUpload(@RequestParam("shipname") String shipname, @RequestParam("file") MultipartFile file){
         if (!file.isEmpty()) {
-            def name = "csb_" + UUID.randomUUID() + ".xyz"
+            def filename = "csb_" + UUID.randomUUID() + ".xyz"
             try {
-                println name
+                println "${shipname} : ${filename}"
                 byte[] bytes = file.getBytes()
                 def stream =
                         new BufferedOutputStream(new FileOutputStream(new File("/tmp/${name}")))
                 stream.write(bytes)
                 stream.close()
-                return "You successfully uploaded ${name}!!"
+                return "You successfully uploaded ${filename}!!"
             } catch (Exception e) {
                 return "You failed to upload ${name}" + e.getMessage()
             }
         } else {
-            return "You failed to upload ${name} because the file was empty."
+            return "You failed to upload ${filename} because the file was empty."
         }
     }
 
