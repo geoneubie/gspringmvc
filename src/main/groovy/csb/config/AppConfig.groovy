9fa1765fed656370f2648f6f61c6d9860b41b78c
geoneubie <geoneubie@gmail.com>
2015-11-29 23:32:36 -1000

    Import cleanup.

--- src/main/groovy/csb/config/AppConfig.groovy
+++ src/main/groovy/csb/config/AppConfig.groovy
@@ -12,7 +12,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.EnableAspectJAutoProxy
-import org.springframework.context.annotation.PropertySource
 
 @Configuration
 @EnableAutoConfiguration
